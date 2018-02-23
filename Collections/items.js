9f59f830879944a64e358c13276b5fa18d161e58
Damien DEMONAZ <damien.demonaz@gmail.com>
2018-02-23 19:47:09 +0100

    Ajout des règles avant/après requêtes pour les éléments de boutique

--- Collections/items.js
+++ Collections/items.js
@@ -39,3 +39,39 @@ Items.attachSchema(new SimpleSchema({
         optional: true
     }
 }));
+
+// On règle la boutique associée à l'objet
+Items.before.insert(function (userId, item){
+    if(item.hasOwnProperty('shop')){    // On affecte l'élément _shop pour l'insertion en BDD
+        item._shop = item.shop._id;
+        delete item.shop;
+    }
+});
+
+Items.before.update(function (userId, item, fieldNames, modifier, options) {
+    if(modifier.$set.hasOwnProperty('shop')){
+        modifier.$set._shop = modifier.$set.shop._id;
+        delete modifier.$set.shop;
+    }
+});
+
+Items.before.upsert(function (userId, selector, modifier, options) {
+    if(modifier.$set.hasOwnProperty('shop')){
+        modifier.$set._shop = modifier.$set.shop._id;
+        delete modifier.$set.shop;
+    }
+});
+
+Items.after.find(function (userId, selector, options, cursor) {
+    cursor.each(function(err, item) {
+        if(item.hasOwnProperty('_shop')){
+            item.shop = Shops.findOne({_id: item._shop});
+        }
+    });
+});
+
+Items.after.findOne(function (userId, selector, options, item) {
+    if(item.hasOwnProperty('_shop')){
+            item.shop = Shops.findOne({_id: item._shop});
+        }
+});
