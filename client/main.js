95b84f7370a057f022e04b8fc1bf6d2c942a294b
Damien DEMONAZ <damien.demonaz@gmail.com>
2018-02-28 23:59:58 +0100

    Working...

--- client/main.js
+++ client/main.js
@@ -8,3 +8,9 @@ import '../imports/ui/pages/import.js';
 import '../imports/ui/components/import.js';
 
 import '../imports/client/comportments/import.js';
+
+// Autoform
+AutoForm.setDefaultTemplateForType('afArrayField', 'bootstrap4');
+AutoForm.setDefaultTemplateForType('quickForm', 'bootstrap4');
+//AutoForm.setDefaultTemplate('bootstrap4');
+//SimpleSchema.debug = true;
